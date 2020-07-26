b1d4352564c8ed65faab71f7493faf532255ae52
easyadin <tobiayokunnu@gmail.com>
2020-07-27 03:53:53 +0400

    threejs

--- src/app/playground/playground.page.ts
+++ src/app/playground/playground.page.ts
@@ -1,62 +1,116 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from  'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.page.html',
   styleUrls: ['./playground.page.scss'],
 })
-export class PlaygroundPage implements OnInit {
-  constructor() { }
+export class PlaygroundPage implements OnInit, AfterViewInit {
+  constructor() {
+  }
 
+  ngAfterViewInit(): void {
+    var dirLight, stats;
+    var mixer, controls;
 
-  ngOnInit() { 
-    // set up scene
+    // create scene
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    // create camera
+    var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(20, -2, 30);
+    camera.lookAt(new THREE.Vector3(0, 0, 0))
+    scene.add(camera);
+
+    // create renderer
     var renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    // add renderer to element
+    const canvas = document.getElementById('main');
+    canvas.appendChild(renderer.domElement);
+
+    scene.background = new THREE.Color(0xcccccc);
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
     // controls
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
 
-    // load headphone model
+    controls.enabled = true;
+    controls.enableRotate = true;
+    controls.enablePan = false;
+    controls.enableZoom = true;
+
+    controls.rotateSpeed = 2
+
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+    controls.screenSpacePanning = true;
+
+    // controls.minDistance = 100;
+    // controls.maxDistance = 500;
+
+    // controls.maxPolarAngle = Math.PI / 2;
+
+    scene.add(new THREE.HemisphereLight(0xffffff, 0x000000, 0.4));
+
+    var light1 = new THREE.DirectionalLight(0xffffff);
+    light1.position.set(1, 1, 1);
+    scene.add(light1);
+
+    var light2 = new THREE.DirectionalLight(0x002288);
+    light2.position.set(- 1, - 1, - 1);
+    scene.add(light2);
+
+    var light3 = new THREE.AmbientLight(0x222222);
+    scene.add(light3);
+
+    dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(0, 0, 8);
+    scene.add(dirLight);
+
     var loader = new GLTFLoader();
 
-    loader.load('../../assets/main/headphone.gltf', function (model) {
-
-      model.scene.traverse(c => {
-        c.castShadow = true;
+    loader.load('../../assets/main/headphone-playground.gltf', function (gltf) {
+      var model = gltf.scene;
+      model.position.set(0, -20, 0);
+      model.traverse(function (child) {
+        child.traverse(c => {
+          c.castShadow = true;
+        });
       });
 
-      // add model
-      scene.add(model.scene);
+      scene.add(model);
 
-      // add light
-      var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.8);
-      scene.add(ambientLight);
+      animate();
 
-      var pointLight1 = new THREE.PointLight(0xFF0040, 4 ,50);
-      scene.add(pointLight1);
+    }, undefined, function (e) {
+      console.error(e);
+    });
 
-      var pointLight2 = new THREE.PointLight(0x0040FF, 3 ,50);
-      scene.add(pointLight2);
-      
-      var pointLight3 = new THREE.PointLight(0x80FF80, 4 ,50);
-      scene.add(pointLight3);
+    // readjust canvas on window resize
+    window.onresize = function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
 
+    // animate
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
       renderer.render(scene, camera);
-    },
-      undefined, function (error) {
-        console.error(error);
-      }
-    );
-
-    renderer.render(scene, camera);
+    }
   }
+
+
+  ngOnInit() { }
 
 }
